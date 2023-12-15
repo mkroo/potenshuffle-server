16d889277c298e672b43e6e33d37c1b0d71a15e7
ansrl0107 <ansrl0107@gmail.com>
2023-12-15 23:40:31 +0900

    Feat: UserInformationResponse 필드 정리

--- src/main/kotlin/best/beside/ctrl/teambuilder/domain/dto/UserInformationResponse.kt
+++ src/main/kotlin/best/beside/ctrl/teambuilder/domain/dto/UserInformationResponse.kt
@@ -1,8 +1,6 @@
 package best.beside.ctrl.teambuilder.domain.dto
 
-import best.beside.ctrl.teambuilder.domain.type.EmploymentStatus
 import best.beside.ctrl.teambuilder.domain.type.Occupation
-import best.beside.ctrl.teambuilder.domain.type.ParticipationPurpose
 import best.beside.ctrl.teambuilder.domain.type.TeamBuildingStatus
 import java.time.LocalDateTime
 
@@ -10,14 +8,8 @@ data class UserInformationResponse(
     val user: User,
     val briefIntroduction: String,
     val introduction: String,
-    val preferredTeamMember: String,
-    val availableParticipationTime: String,
-    val employmentStatus: EmploymentStatus,
     val occupation: Occupation,
-    val participationPurpose: ParticipationPurpose,
     val keywords: List<String>,
-    val strengths: List<String>,
-    val skills: List<String>,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime,
 ) {
