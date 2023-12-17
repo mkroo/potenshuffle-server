39e0770f48e54f419e392eebf40127e69472eb28
ansrl0107 <ansrl0107@gmail.com>
2023-12-17 10:33:22 +0900

    Chore: 팀 제안 API Controller 통합

--- src/main/kotlin/best/beside/ctrl/teambuilder/presentation/TeamOfferController.kt
+++ src/main/kotlin/best/beside/ctrl/teambuilder/presentation/TeamOfferController.kt
@@ -1,18 +1,23 @@
 package best.beside.ctrl.teambuilder.presentation
 
+import best.beside.ctrl.teambuilder.application.ReceivedTeamOfferService
+import best.beside.ctrl.teambuilder.application.SentTeamOfferService
 import best.beside.ctrl.teambuilder.application.TeamOfferService
 import best.beside.ctrl.teambuilder.domain.dto.TeamOfferParams
 import best.beside.ctrl.teambuilder.domain.dto.TeamOfferResponse
+import best.beside.ctrl.teambuilder.domain.type.TeamOfferResponseType
 import best.beside.ctrl.teambuilder.domain.valueobject.Account
+import best.beside.ctrl.teambuilder.domain.valueobject.PageResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.data.domain.PageRequest
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class TeamOfferController(
     private val teamOfferService: TeamOfferService,
+    private val sentTeamOfferService: SentTeamOfferService,
+    private val receivedTeamOfferService: ReceivedTeamOfferService,
 ) {
     @PostMapping("/users/me/team-offers")
     @SecurityRequirement(name = "JsonWebToken")
@@ -21,5 +26,43 @@ class TeamOfferController(
         @RequestBody params: TeamOfferParams,
     ): TeamOfferResponse.Sent {
         return teamOfferService.sendTeamOffer(account.userId, params)
+    }
+
+    @GetMapping("/users/me/sent-team-offers")
+    @SecurityRequirement(name = "JsonWebToken")
+    fun listSentTeamOffers(
+        @AuthenticationPrincipal account: Account,
+        @RequestParam(required = false, defaultValue = "0") page: Int,
+        @RequestParam(required = false, defaultValue = "10") size: Int,
+    ): PageResponse<TeamOfferResponse.Sent> {
+        val pageable = PageRequest.of(page, size)
+
+        return sentTeamOfferService.listSentTeamOffers(account.userId, pageable)
+    }
+
+    @GetMapping("/users/me/received-team-offers")
+    @SecurityRequirement(name = "JsonWebToken")
+    fun getReceivedTeamOffers(
+        @AuthenticationPrincipal account: Account,
+        @RequestParam(required = false, defaultValue = "0") page: Int,
+        @RequestParam(required = false, defaultValue = "10") size: Int,
+    ): PageResponse<TeamOfferResponse.Received> {
+        val pageable = PageRequest.of(page, size)
+
+        return receivedTeamOfferService.listReceivedTeamOffers(account.userId, pageable)
+    }
+
+    data class ReceivedJobOfferResponseParams(
+        val responseType: TeamOfferResponseType,
+    )
+
+    @PostMapping("/received-team-offers/{teamOfferId}/response")
+    @SecurityRequirement(name = "JsonWebToken")
+    fun respondReceivedTeamOffer(
+        @AuthenticationPrincipal account: Account,
+        @RequestBody params: ReceivedJobOfferResponseParams,
+        @PathVariable teamOfferId: Long,
+    ) {
+        receivedTeamOfferService.respondReceivedTeamOffer(account.userId, teamOfferId, params.responseType)
     }
 }
