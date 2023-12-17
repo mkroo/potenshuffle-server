43ced480651dad179880bbf21838dc332279dde7
ansrl0107 <ansrl0107@gmail.com>
2023-12-17 14:21:28 +0900

    Fix: 챗봇 응답 비교 오류 수정

--- src/main/kotlin/best/beside/ctrl/teambuilder/infrastructure/ncp/service/ClovaStudioIntentDistinctionService.kt
+++ src/main/kotlin/best/beside/ctrl/teambuilder/infrastructure/ncp/service/ClovaStudioIntentDistinctionService.kt
@@ -12,19 +12,24 @@ class ClovaStudioIntentDistinctionService(
 ) {
     private val modelName = properties.chatCompletion.modelName
     private val apiKey = properties.chatCompletion.apiKey
-    private val systemMessage = ChatCompletionRequest.SystemMessage("네가 물어본 질문의 의도에 알맞은 대답을 했는지 '예', '아니요'로만 대답해")
+    private val systemMessage = ChatCompletionRequest.SystemMessage(
+        """
+            - 네가 물어본 질문에 의도에 알맞은 대답인지 판단한다.
+            - 대답은 "예", "아니오"로만 한다.
+        """.trimIndent()
+    )
 
     fun isAppropriateAnswer(question: String, answer: String): Boolean {
         val botMessage = ChatCompletionRequest.AssistantMessage(question)
         val userMessage = ChatCompletionRequest.UserMessage(answer)
 
         val request = ChatCompletionRequest(
-            messages = listOf(systemMessage, botMessage, userMessage)
+            messages = listOf(systemMessage, botMessage, userMessage),
+            temperature = 0.1
         )
 
         val response = clovaStudioClient.chatCompletion(modelName, apiKey, request)
 
-
-        return response.result.message.content === "예"
+        return response.result.message.content == "예"
     }
 }
